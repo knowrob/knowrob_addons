846118097ce36fb9116acadd3274783891cac923
Moritz Tenorth <tenorth@cs.tum.edu>
2009-10-16 16:29:56 +0200

    fixed quoting problem with library includes

--- prolog/comp_ehow.pl
+++ prolog/comp_ehow.pl
@@ -35,10 +35,10 @@
       comp_forCommand/2
     ]).
 
-:- use_module(library(semweb/rdfs)).
-:- use_module(library(semweb/rdf_db)).
-:- use_module(library(semweb/rdfs_computable)).
-:- use_module(library(jpl)).
+:- use_module(library('semweb/rdfs')).
+:- use_module(library('semweb/rdf_db')).
+:- use_module(library('semweb/rdfs_computable')).
+:- use_module(library('jpl')).
 
 
 
