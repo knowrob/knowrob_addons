1f0424d1a3f6a71787d91c2a9623c690a77901d2
Moritz Tenorth <tenorth@cs.tum.edu>
2010-07-06 09:55:40 +0200

    visualization predicates

--- prolog/comp_missingobj.pl
+++ prolog/comp_missingobj.pl
@@ -26,11 +26,11 @@
 
 :- module(comp_missingobj,
     [
-      vis_all_objects_on_table/1,
-      vis_all_objects_inferred/3,
+%       vis_all_objects_on_table/1,
+%       vis_all_objects_inferred/3,
       comp_missingObjectTypes/2
     ]).
-    
+
 :-  rdf_meta
     comp_missingObjectTypes(r, r),
     current_objects_on_table(r,r).
@@ -47,131 +47,61 @@
 % should be, compare both and create instances of the missing items with an existence
 % probability as determined by the prob inference procedure
 %
-comp_missingObjectTypes(T, MissingTypes) :-
+% @param Table        Instance of a KitchenTable on which the setup is to be completed
+% @param MissingTypes List of object types that should be on the table given the current setup
+%
+comp_missingObjectTypes(Table, MissingTypes) :-
 
   get_timepoint(NOW),
 
-  % find detected objects on table T
-  findall(Curr, holds(on_Physical(Curr, T), NOW), DetectedObjects),
+  % find detected objects on table Table
+  findall(Curr, holds(on_Physical(Curr, Table), NOW), PerceivedObjects),
 
   % call the probabilistic inference engine
-  mod_probcog_tablesetting:required_objects(RequiredObjects),
+  mod_probcog_tablesetting:required_objects(_),
 
-  compute_missing_objects(DetectedObjects, RequiredObjects, MissingTypes).
+  compute_missing_objects(PerceivedObjects, MissingTypes).
 
 
 
-%% compute_missing_objects(+AreOnTable, +ShouldBeOnTableWithProb, -Missing, -MissingTypes)
+%% compute_missing_objects(+PerceivedObjects, -MissingTypes) is nondet.
 %
-% compare the list of objects that are already on the table with that
-% of objects that are supposed to be there to find missing items,
-% create instances of these objects with the resp. probability
+% Compare the list of objects that are already on the table with those
+% objects that are supposed to be there to find missing items.
 %
-compute_missing_objects(DetectedObjects, _RequiredObjects, MissingTypes) :-
+% @param PerceivedObjects  List of objects detected on a table
+% @param MissingTypes      List of object types that should be on that table given the current setup
+%
+compute_missing_objects(PerceivedObjects, MissingTypes) :-
 
   % get sorted list of elements that have a prob > 0 to be on the table
   latest_inferred_object_set(RequiredObjectTypes),
 
-
   % read types of objects on the table
-  findall(T, (member(O, DetectedObjects), rdf_has(O, rdf:type, T)), DetectedObjectTypes),
+  findall(T, (member(O, PerceivedObjects), rdf_has(O, rdf:type, T)), DetectedObjectTypes),
 
   % find all objects that should be on the table, but are not member of the set of observed objects
   findall(ReqO, (member(ReqO, RequiredObjectTypes), not(member(ReqO, DetectedObjectTypes))), MissingTypes).
 
 
-% TODO:
 
-% * copID
-% * jloID
-% * query for last detection of an instance of the respective type
-%
-
-
-% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
-% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
-% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
-% % % 
-% % %
-% % % TODO: update and remove the following
-% % %
-% % % 
-
-
-
+% backwards compatibility
 current_objects_on_table(T, Curr) :-
-get_timepoint(NOW),
-holds(on_Physical(Curr, T), NOW).
-
-mostLikelyObjects(Objs, Probs, InfObjs) :-
-  findall(InfObj, (member(O,Objs),
-                    rdf_has(O,rdf:type,T),
-                    member(InfObj,Probs),
-                    rdf_has(InfObj,rdf:type,T)), InfObjs).
+  objects_on_table(_, _),
+  get_timepoint(NOW),
+  holds(on_Physical(Curr, T), NOW).
 
 
-mostLikelyObj(Objs, Probs, MLObj) :-
-  mostLikelyObjects(Objs, Probs, InfObjs),
-  predsort(compare_inference_probs2, InfObjs, [MLInfObj|_]),
-  rdf_has(MLInfObj,rdf:type,Type),
-  member(MLObj,Objs),
-  rdf_has(MLObj,rdf:type,Type).
+% visualize all tables in the environment and all objects
+% that are perceived on top of one of these tables
+visualize_perceived_objects(PerceivedObjects) :-
 
-
-% mostLikelyObjCopID(Objs, Probs, MLObj, CID) :-
-%   mostLikelyObj(Objs, Probs, MLObj),
-%   rdf_has(MLObj, knowrob:copID, CID).
-
-
-% inferMissingObjects(Instances) :-
-%    objects_on_table(_,_),!,
-%    required_objects(Instances).
-
-
-missingObject(Table, MLObj, CID):-
-     findall(Obj, current_objects_on_table(Table, Obj), Objs),
-     findall(InfObj, (rdf_has(Inf, rdf:type, knowrob:'TableSettingModelInference'), rdf_has(Inf,knowrob:objectActedOn,InfObj)), Probs),
-     mostLikelyObjCopID(Objs, Probs, MLObj, CID).
-
-
-vis_all_objects_on_table(Table):-
+  rdfs_instance_of(Table, knowrob:'KitchenTable'),
   add_object_perception(Table, _),
-  current_objects_on_table(Table, O),
-  add_object_perception(O, _).
 
-vis_all_objects_inferred(T,O,P):-
-  currently_inferred_objects(O),
-  rdf_has(Inf,knowrob:objectActedOn,O),
-  rdf_has(Inf, rdf:type, knowrob:'TableSettingModelInference'),
-  rdf_has(Inf,knowrob:probability,P),
-  term_to_atom(N,P),
-  N>T,
-  add_object_perception(O,  _).
+  findall(Obj, (member(Obj, PerceivedObjects), add_object_perception(Obj, _)), _).
 
 
-
-
-
-% % %% compare_inference_probs2(-Delta, +P1, +P2)
-% % %
-% % % compare two object classes with their resp. probabilities
-% % %
-% % compare_inference_probs2('>', I1, I2) :-
-% %     probability_of_instance(I1,P1),
-% %     probability_of_instance(I2,P2),
-% %     term_to_atom(N1, P1),
-% %     term_to_atom(N2, P2),
-% %     N1 < N2.
-% % 
-% % compare_inference_probs2('<', I1, I2) :-
-% %     probability_of_instance(I1,P1),
-% %     probability_of_instance(I2,P2),
-% %     term_to_atom(N1, P1),
-% %     term_to_atom(N2, P2),
-% %     N1>=N2.
-% % 
-% % 
-% % probability_of_instance(I,P):-
-% %    rdf_has(Inf,knowrob:objectActedOn,I),
-% %    rdf_has(Inf,knowrob:probability, P).
+visualize_missing_objects(MissingObjects) :-
+  findall(Obj, (member(Obj, MissingObjects), add_object_perception(Obj, _)), _).
 
