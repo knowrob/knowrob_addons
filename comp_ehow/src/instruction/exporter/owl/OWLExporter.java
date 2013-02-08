aa568898b86b7116b20f256e5eadc6a55a7d5951
Moritz Tenorth <tenorth@cs.tum.edu>
2013-02-08 11:03:03 +0000

    added ordering constraints to OWL exporter

--- comp_ehow/src/instruction/exporter/owl/OWLExporter.java
+++ comp_ehow/src/instruction/exporter/owl/OWLExporter.java
@@ -16,6 +16,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -25,15 +26,18 @@ import java.util.Map;
 import java.util.Set;
 
 import edu.stanford.smi.protege.exception.OntologyLoadException;
+import edu.stanford.smi.protege.model.Instance;
 import edu.stanford.smi.protegex.owl.ProtegeOWL;
 import edu.stanford.smi.protegex.owl.model.OWLClass;
-import edu.stanford.smi.protegex.owl.model.OWLDatatypeProperty;
+import edu.stanford.smi.protegex.owl.model.OWLHasValue;
 import edu.stanford.smi.protegex.owl.model.OWLIndividual;
+import edu.stanford.smi.protegex.owl.model.OWLDatatypeProperty;
 import edu.stanford.smi.protegex.owl.model.OWLModel;
 import edu.stanford.smi.protegex.owl.model.OWLNamedClass;
 import edu.stanford.smi.protegex.owl.model.OWLObjectProperty;
 import edu.stanford.smi.protegex.owl.model.OWLSomeValuesFrom;
 import edu.stanford.smi.protegex.owl.model.ProtegeInstance;
+import edu.stanford.smi.protegex.owl.model.RDFIndividual;
 import edu.stanford.smi.protegex.owl.model.RDFSClass;
 import edu.stanford.smi.protegex.owl.writer.rdfxml.rdfwriter.OWLModelWriter;
 
@@ -42,7 +46,7 @@ public class OWLExporter {
 	static {
 		ConfigurationManager.loadSettings();
 	}
-	
+
 	public static final String KNOWROB_URI = ConfigurationManager.getPathKnowRob();
 	public static final String ABSTRACT_ACTION_CLASS = "knowrob:Action";
 	public static final String ABSTRACT_OBJECT_CLASS = "knowrob:SpatialThing-Localized";
@@ -56,19 +60,21 @@ public class OWLExporter {
 	private static int locationCounter = 1;
 
 	private PlanImporter importer;
+	private OWLModel knowrobOwlModel = null;
+
 
 	public OWLExporter() {
-		
+
 		try {
-			
+
 			importer = new PlanImporter();
 			importer.initialize();
-			
+
 			this.initOWLConverter();
-			
+
 			System.out.println("Initializing Plan-Importer...");
 			Map<String, List<String>> mappings = ConfigurationManager.getMappings();
-			
+
 			Set<String> synsets = mappings.keySet();
 			for (Iterator<String> i = synsets.iterator(); i.hasNext();) {
 				String synset = i.next();
@@ -80,7 +86,7 @@ public class OWLExporter {
 			importer.getDisambiguator().load(
 					ConfigurationManager.getPathDisambiguator());
 			System.out.println("Plan-Importer initialized.");
-			
+
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		} catch (InstructionException e) {
@@ -91,13 +97,13 @@ public class OWLExporter {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public OWLExporter(PlanImporter imp) {
 		try {
 
 			this.importer = imp;
 			this.initOWLConverter();
-			
+
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		} catch (InstructionException e) {
@@ -108,9 +114,9 @@ public class OWLExporter {
 			e.printStackTrace();
 		}
 	}
-	
-	
-	
+
+
+
 	/**
 	 * Look up instructions for the command 'howto' and translate them into a formal OWL representation
 	 * 
@@ -150,8 +156,8 @@ public class OWLExporter {
 		return null;
 	}
 
-	
-	
+
+
 	/**
 	 * Look up instructions for the command 'howto' and translate them into a formal OWL representation
 	 * 
@@ -173,12 +179,12 @@ public class OWLExporter {
 
 			if (!owlFile.exists())
 				owlFile.createNewFile();
-			
+
 			System.out.println("Writing OWL file...");
 			FileWriter writer = new FileWriter(owlFile);
 			writer.write(owlContent);
 			writer.close();
-			
+
 			return owlFile.getAbsolutePath();
 
 
@@ -190,11 +196,11 @@ public class OWLExporter {
 
 		return null;
 	}
-	
-	
-	
-	
-	
+
+
+
+
+
 	/**
 	 * Initialize the converter, including initialization of the OpenCyc
 	 * interface and parsing the WordNet ontology.
@@ -206,23 +212,25 @@ public class OWLExporter {
 	 * @throws IOException
 	 */
 	private PlanImporter initOWLConverter() throws InstructionException,
-			Exception, UnknownHostException, IOException {
-		
+	Exception, UnknownHostException, IOException {
+
+		knowrobOwlModel = ProtegeOWL.createJenaOWLModelFromURI(KNOWROB_URI);
+
 		readAvailableActions();
 		readAvailableObjects();
 		readAvailableProperties();
 
 		ConfigurationManager.loadSettings();
 		ConfigurationManager
-				.setPathDisambiguator("./etc/disambiguator.xml");
+		.setPathDisambiguator("./etc/disambiguator.xml");
 		ConfigurationManager.setPathHowtos("./howtos");
 
 		return importer;
 	}
 
-	
-	
-	
+
+
+
 	/**
 	 * Convert a list of instructions into OWL
 	 * 
@@ -232,13 +240,13 @@ public class OWLExporter {
 	 * @return OWL representation of the task
 	 * @throws InstructionException
 	 */
-	
+
 	private String convertInstructionsToOWL(List<Instruction> inst,
 			Instruction title, String howtoName) throws InstructionException {
 
 		try {
 			OWLModel owlModel = ProtegeOWL.createJenaOWLModelFromURI("http://ias.cs.tum.edu/kb/ehow_input.owl");
-			
+
 			int actionCounter = 1;
 
 			// Create ComplexTask
@@ -246,39 +254,42 @@ public class OWLExporter {
 			// Create an easy to read task name composed by the howto name
 			String[] howtoTokens = howtoName.split(" ");
 			String task = "";
-			Set<ProtegeInstance> subTasks = new HashSet<ProtegeInstance>();
+			List<ProtegeInstance> subTaskRestr = new ArrayList<ProtegeInstance>();
+			List<OWLNamedClass> subTasks = new ArrayList<OWLNamedClass>();
 
 			for (String t : howtoTokens)
 				task += t.substring(0, 1).toUpperCase() + t.substring(1);
 
-			OWLNamedClass complexTask = owlModel.createOWLNamedSubclass(task,
-					actionStore.get("Action"));
+			OWLNamedClass complexTask = owlModel.createOWLNamedSubclass(task, actionStore.get("Action"));
 
 			// add the howto name as a label so that the plan can be found from Prolog
 			complexTask.addLabel(howtoName, null);
-			
+
+
+			// create action classes
 			for (Iterator<Instruction> i = inst.iterator(); i.hasNext();) {
+
 				Instruction in = i.next();
 
-				String action = in.getAction().getAction().getCycConcepts()
-						.get(0);
+				String action = in.getAction().getAction().getCycConcepts().get(0);
 
 				if(action.equals("Flipping")) {
 					action = "FlippingAnObject";
 				}
-				
+
 				OWLNamedClass owlClass = actionStore.get(action);
 
 				if (owlClass != null) {
 
 					// Create action subclass
-					OWLNamedClass actionClass = owlModel
-							.createOWLNamedSubclass(action + actionCounter++, owlClass);
+					OWLNamedClass actionClass = owlModel.createOWLNamedSubclass(action + actionCounter++, owlClass);
 
+					actionClass.addLabel(in.getNLSentence(), null);
+					subTasks.add(actionClass);
+					
 					// Set the restriction on the "subEvents" property of the
 					// complex task
-					OWLSomeValuesFrom value = owlModel.createOWLSomeValuesFrom(
-							objPropStore.get("subAction"), actionClass);
+					OWLSomeValuesFrom value = owlModel.createOWLSomeValuesFrom(objPropStore.get("subAction"), actionClass);
 
 					// Set the restriction on the "objectActedOn" property
 					List<ObjectX> objects = in.getObjects();
@@ -287,50 +298,47 @@ public class OWLExporter {
 					objectRestrictions.add(owlClass);
 
 					for (Iterator<ObjectX> i2 = objects.iterator(); i2.hasNext();) {
-						
+
 						ObjectX o = i2.next();
 						String objectDesignator = o.getName().get(0).getCycConcepts().get(0);
 
 						String mappedName = objectDesignator;
 						if (objectStore.get(mappedName) != null) {
 							OWLSomeValuesFrom objectActedOn = owlModel.createOWLSomeValuesFrom(objPropStore
-																		.get("objectActedOn"), objectStore
-																		.get(mappedName));
+									.get("objectActedOn"), objectStore
+									.get(mappedName));
 							objectRestrictions.add(objectActedOn);
 						} else
 							System.err.println("Unkown Object Class: " + mappedName);
 					}
 
-					
+
 					// handle prepositional constraints
 					if (in.getPrepositions().size() > 0) {
 						Preposition p = in.getPrepositions().get(0);
 						if (p.getObjects().size() > 0) {
-							OWLNamedClass locations = getSpatialPostconditions(
-									p.getObjects().get(0), owlModel);
+							OWLNamedClass locations = getSpatialPostconditions(p.getObjects().get(0), owlModel);
+
 							if (locations != null && (p.getPrepositions().contains(new Word( Word.TYPE_PREPOSITION, "from")) ||
 									p.getPrepositions().contains(new Word( Word.TYPE_PREPOSITION, "off"))))
-								objectRestrictions.add(owlModel
-										.createOWLSomeValuesFrom(objPropStore
-												.get("fromLocation"), locations));
+								objectRestrictions.add(owlModel.createOWLSomeValuesFrom(objPropStore.get("fromLocation"), locations));
+
 							else if (locations != null)
-								objectRestrictions.add(owlModel
-										.createOWLSomeValuesFrom(objPropStore
-												.get("toLocation"), locations));
+								objectRestrictions.add(owlModel.createOWLSomeValuesFrom(objPropStore.get("toLocation"), locations));
 						}
 					}
 
 					// handle time constraints in the instructions
 					if (in.getTimeConstraint() != null) {
-						
+
 						Quantifier q = in.getTimeConstraint();
 						if (q.getMeasure() != null) {
-							
+
 							float duration = Float.valueOf(q.getAlternatives().get(0).getLabel());
 
 							if(q.getMeasure().getLabel().contains("inutes")) {
 								duration *= 60;
-								
+
 							} else if (q.getMeasure().getLabel().contains("ours")) {
 								duration *= 3600;
 
@@ -342,23 +350,51 @@ public class OWLExporter {
 					}
 
 					// Add the restrictions
-					actionClass.addEquivalentClass(owlModel
-							.createOWLIntersectionClass(objectRestrictions));
+					actionClass.addEquivalentClass(owlModel.createOWLIntersectionClass(objectRestrictions));
+
 					// Add the new action class as subEvent to the ComplexTask
-					subTasks.add(value);
+					subTaskRestr.add(value);
+
 				} else
 					System.err.println("Unknown Action Class: " + action);
 			}
 
-			complexTask.addEquivalentClass(owlModel
-					.createOWLIntersectionClass(subTasks));
 
+			// add ordering constraints
+			List<ProtegeInstance> orderingConstraints = new ArrayList<ProtegeInstance>();
+			OWLNamedClass partialOrderStrict = owlModel.createOWLNamedClass("knowrob:PartialOrdering-Strict");
+			OWLObjectProperty before = owlModel.createOWLObjectProperty("knowrob:occursBeforeInOrdering");
+			OWLObjectProperty after = owlModel.createOWLObjectProperty("knowrob:occursAfterInOrdering");
+			
+			
+			for(int a=0; a<subTasks.size(); a++) {
+
+				ProtegeInstance act1 = subTasks.get(a);
+
+				// iterate over all subsequent actions in the task and add the precedence relation
+
+				for(int b=a+1; b<subTasks.size(); b++) {
+
+					ProtegeInstance act2 = subTasks.get(b);
+					RDFIndividual constr = partialOrderStrict.createRDFIndividual("PartialOrderConstr" + actionCounter++);
+
+					constr.addPropertyValue(before, act1);
+					constr.addPropertyValue(after, act2);
+
+					// add restriction on knowrob:orderingConstraints to complexTask
+					orderingConstraints.add(owlModel.createOWLHasValue(objPropStore.get("orderingConstraints"), constr));
+				}
+
+			}
+
+			subTaskRestr.addAll(orderingConstraints);
+			complexTask.addEquivalentClass(owlModel.createOWLIntersectionClass(subTaskRestr));
+
+
+			// write to string
 			StringWriter writer = new StringWriter();
-
-			OWLModelWriter modelWwriter = new OWLModelWriter(owlModel, owlModel
-					.getTripleStoreModel().getActiveTripleStore(), writer);
-
-			modelWwriter.write();
+			OWLModelWriter modelWriter = new OWLModelWriter(owlModel, owlModel.getTripleStoreModel().getActiveTripleStore(), writer);
+			modelWriter.write();
 			writer.close();
 
 			return writer.toString();
@@ -370,7 +406,7 @@ public class OWLExporter {
 		}
 	}
 
-	
+
 
 
 	/**
@@ -379,19 +415,15 @@ public class OWLExporter {
 	 * @throws InstructionException
 	 */
 	@SuppressWarnings("unchecked")
-	private static void readAvailableActions() throws InstructionException {
+	private void readAvailableActions() throws InstructionException {
 
 		try {
-			OWLModel owlModel = ProtegeOWL
-					.createJenaOWLModelFromURI(KNOWROB_URI);
 
-			OWLNamedClass actionClass = owlModel
-					.getOWLNamedClass(ABSTRACT_ACTION_CLASS);
+			OWLNamedClass actionClass = knowrobOwlModel.getOWLNamedClass(ABSTRACT_ACTION_CLASS);
 
 			actionStore.put(actionClass.getLocalName(), actionClass);
 
-			Collection<OWLNamedClass> classes = owlModel
-					.getUserDefinedOWLNamedClasses();
+			Collection<OWLNamedClass> classes = knowrobOwlModel.getUserDefinedOWLNamedClasses();
 
 			for (Iterator<OWLNamedClass> it = classes.iterator(); it.hasNext();) {
 				OWLNamedClass clazz = it.next();
@@ -411,18 +443,13 @@ public class OWLExporter {
 	 * @throws InstructionException
 	 */
 	@SuppressWarnings("unchecked")
-	private static void readAvailableObjects() throws InstructionException {
+	private void readAvailableObjects() throws InstructionException {
 
 		try {
 
-			OWLModel owlModel = ProtegeOWL
-					.createJenaOWLModelFromURI(KNOWROB_URI);
+			OWLClass actionClass = knowrobOwlModel.getOWLNamedClass(ABSTRACT_OBJECT_CLASS);
 
-			OWLClass actionClass = owlModel
-					.getOWLNamedClass(ABSTRACT_OBJECT_CLASS);
-
-			Collection<OWLNamedClass> classes = owlModel
-					.getUserDefinedOWLNamedClasses();
+			Collection<OWLNamedClass> classes = knowrobOwlModel.getUserDefinedOWLNamedClasses();
 
 			for (Iterator<OWLNamedClass> it = classes.iterator(); it.hasNext();) {
 				OWLNamedClass clazz = it.next();
@@ -444,33 +471,24 @@ public class OWLExporter {
 	 * @throws InstructionException
 	 */
 	@SuppressWarnings("unchecked")
-	private static void readAvailableProperties() 	throws InstructionException {
+	private void readAvailableProperties() 	throws InstructionException {
 
-		try {
+		Collection<OWLObjectProperty> obj_properties = knowrobOwlModel.getUserDefinedOWLObjectProperties();
 
-			OWLModel owlModel = ProtegeOWL.createJenaOWLModelFromURI(KNOWROB_URI);
+		for (Iterator<OWLObjectProperty> it = obj_properties.iterator(); it.hasNext();) {
+			OWLObjectProperty prop = it.next();
+			objPropStore.put(prop.getLocalName(), prop);
+		}
 
-			Collection<OWLObjectProperty> obj_properties = owlModel.getUserDefinedOWLObjectProperties();
+		Collection<OWLDatatypeProperty> data_properties = knowrobOwlModel.getUserDefinedOWLDatatypeProperties();
 
-			for (Iterator<OWLObjectProperty> it = obj_properties.iterator(); it.hasNext();) {
-				OWLObjectProperty prop = it.next();
-				objPropStore.put(prop.getLocalName(), prop);
-			}
-
-			Collection<OWLDatatypeProperty> data_properties = owlModel.getUserDefinedOWLDatatypeProperties();
-
-			for (Iterator<OWLDatatypeProperty> it = data_properties.iterator(); it.hasNext();) {
-				OWLDatatypeProperty prop = it.next();
-				dataPropStore.put(prop.getLocalName(), prop);
-			}
-
-			
-		} catch (OntologyLoadException e) {
-			e.printStackTrace();
+		for (Iterator<OWLDatatypeProperty> it = data_properties.iterator(); it.hasNext();) {
+			OWLDatatypeProperty prop = it.next();
+			dataPropStore.put(prop.getLocalName(), prop);
 		}
 	}
 
-	
+
 	/**
 	 * Return spatial post-conditions for the ObjectX obj
 	 * @param obj
@@ -510,11 +528,11 @@ public class OWLExporter {
 					OWLObjectProperty spatialRelation = null;
 					if (objectPredicate.equalsIgnoreCase("FrontSide"))
 						spatialRelation = objPropStore
-								.get("inFrontOf-Generally");
-					
+						.get("inFrontOf-Generally");
+
 					else if (objectPredicate.equalsIgnoreCase("TopSide"))
 						spatialRelation = objPropStore.get("aboveOf");
-					
+
 					else if (objectPredicate.equalsIgnoreCase("BottomSide"))
 						spatialRelation = objPropStore.get("belowOf");
 
