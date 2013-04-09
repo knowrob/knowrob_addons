16b8cc93945b5b06ff971ee11f17dea377588490
Moritz Tenorth <tenorth@cs.tum.edu>
2013-04-09 22:55:10 +0200

    added query for reading constraint properties to test client

--- knowrob_motion_constraints/src/json_prolog_client.py
+++ knowrob_motion_constraints/src/json_prolog_client.py
@@ -9,7 +9,17 @@ if __name__ == '__main__':
     rospy.init_node('test_json_prolog')
     prolog = json_prolog.Prolog()
     
-    query = prolog.query(" plan_constraint_templates(pancake_constr:'FlippingAPancake', _Cs), member(C, _Cs), constraint_properties(C, P, O)")
+    print("Read constraint templates:")
+    query = prolog.query("plan_constraint_templates(pancake_constr:'FlippingAPancake', _Cs), member(C, _Cs), constraint_properties(C, P, O)")
     for solution in query.solutions():
         print 'Found solution:\n C = %s\n P = %s\n O = %s\n' % (solution['C'], solution['P'], solution['O'])
     query.finish()
+
+    print "\n\n\n\n\n"
+    
+    print("Read the actual sets of constraints and their properties:")
+    query = prolog.query("plan_constraints(pancake_constr:'FlippingAPancake', C), owl_direct_subclass_of(C, T), rdf_has(T, rdf:type, owl:'Class'),  constraint_properties(C, P, O).")
+    for solution in query.solutions():
+        print 'Found solution:\n C = %s\n P = %s\n O = %s\n' % (solution['C'], solution['P'], solution['O'])
+    query.finish()
+    
