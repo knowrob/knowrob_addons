7ab9d456eb001c67075517f4ced9fc499a2800c4
asilx <asil@cs.uni-bremen.de>
2013-08-20 14:28:30 +0200

    introduced failure predicates plus several fixes for bugs which are observed during testing with new logs

--- mod_execution_trace/prolog/comp_execution_trace.pl
+++ mod_execution_trace/prolog/comp_execution_trace.pl
@@ -1,4 +1,4 @@
-:- module(comp_self_info,
+:- module(comp_execution_trace,
     [
      	task/1,
 	subtask/2,
@@ -6,14 +6,19 @@
       	task_goal/2,
 	task_start/2,
 	task_end/2,
-	holds/2,
+	cram_holds/2,
 	returned_value/2,
 	computable_belief/3,
       	computable_truth/3,
 	computable_time_check/3,
 	computable_location_check/3,
+	computable_perception_time_instances/2,
+	computable_perception_object_instances/2,
 	belief_at/2,
-	truth_at/2
+	truth_at/2,
+	failure_class/2,
+	failure_task/2,
+	failure_attribute/3
 	%task_status done
 	%belief_at done
 	%occur
@@ -29,6 +34,37 @@
 
 :- rdf_db:rdf_register_ns(knowrob,  'http://ias.cs.tum.edu/kb/knowrob.owl#',  [keep(true)]).
 :- rdf_db:rdf_register_ns(modexecutiontrace, 'http://ias.cs.tum.edu/kb/knowrob_cram.owl#', [keep(true)]).
+
+% define holds as meta-predicate and allow the definitions
+% to be in different parts of the source file
+:- meta_predicate cram_holds(0, ?, ?).
+:- discontiguous cram_holds/2.
+
+
+% define predicates as rdf_meta predicates
+% (i.e. rdf namespaces are automatically expanded)
+:-  rdf_meta
+    task(r),
+    subtask(r,r),
+    subtask_all(r,r),
+    task_goal(r,r),
+    task_start(r,r),
+    task_end(r,r),
+    cram_holds(r,r),
+    returned_value(r,r),
+    computable_belief(r,r,r),
+    computable_truth(r,r,r),
+    computable_time_check(r,r,r),
+    computable_location_check(r,r,r),
+    computable_perception_time_instance(r,r),
+    computable_perception_object_instance(r,r),
+    belief_at(r,r),
+    truth_at(r,r),
+    failure_class(r,r),
+    failure_task(r,r),
+    failure_attribute(r,r,r).
+
+
 
 task(Task) :-	
 	rdf_has(Task, rdf:type, A),
@@ -88,21 +124,21 @@ task_end(Task, End) :-
 	task(Task),
 	rdf_has(Task, knowrob:'endTime', End).
 
-holds(task_status(Task, Status), T):-
+cram_holds(task_status(Task, Status), T):-
 	nonvar(Task),	
 	task(Task),
 	task_start(Task, Start),
 	task_end(Task, End),
 	computable_time_check(Start, T, Compare_Result1),
-	computable_time_check(T, End, Compare_Result2)
+	computable_time_check(T, End, Compare_Result2),
 	term_to_atom(Compare_Result1, c1),	
 	term_to_atom(Compare_Result2, c2),
-	((c1 is 1) -> (((c2 is 1) -> (Status = [Continue]);(Status = [Done])));(((c2 is 1) -> (Status = [Error]); (Status = [NotStarted]))));
+	((c1 is 1) -> (((c2 is 1) -> (Status = ['Continue']);(Status = ['Done'])));(((c2 is 1) -> (Status = ['Error']); (Status = ['NotStarted']))));
 
 	nonvar(Status),
-	nonvar(T),.
+	nonvar(T).
 
-holds(object_visible(Object, Status), T):-
+cram_holds(object_visible(Object, Status), T):-
 	nonvar(Object),	
 	nonvar(T),
 	computable_belief(Object, T, Loc),
@@ -120,7 +156,7 @@ holds(object_visible(Object, Status), T):-
 	computable_perception_object_instances(T, Object),
 	Status = [true].	
 
-holds(object_placed_at(Object, Loc), T):-
+cram_holds(object_placed_at(Object, Loc), T):-
 	computable_belief(Object, T, Actual_Loc),
 	computable_time_check(Loc, Actual_Loc, Compare_Result),
 	term_to_atom(Compare_Result, r),
@@ -141,16 +177,13 @@ computable_belief(Object, Time, Loc) :-
     % create ROS client object
     jpl_new('edu.tum.cs.ias.knowrob.mod_execution_trace.ROSClient_low_level', ['my_low_level'], Client),
 
-  
-
     jpl_call(Client, 'getBelief', [], Localization_Array),
 
     jpl_array_to_list(Localization_Array, LocList),
 
-
     [M00, M01, M02, M03, M10, M11, M12, M13, M20, M21, M22, M23, M30, M31, M32, M33] = LocList,
 
-    atomic_list_concat(['rotMat3D_',M00,'_',M01,'_',M02,'_',M03,'_',M10,'_',M11,'_',M12,'_',M13,'_',M20,'_',M21,'_',M22'_',M23,'_',M30,'_',M31,'_',M32'_',M33], LocIdentifier),
+    atomic_list_concat(['rotMat3D_',M00,'_',M01,'_',M02,'_',M03,'_',M10,'_',M11,'_',M12,'_',M13,'_',M20,'_',M21,'_',M22,'_',M23,'_',M30,'_',M31,'_',M32,'_',M33], LocIdentifier),
 
     atom_concat('http://ias.cs.tum.edu/kb/knowrob.owl#', LocIdentifier, Loc),
     rdf_assert(Loc, rdf:type, knowrob:'RotationMatrix2D').
@@ -168,10 +201,9 @@ computable_truth(Object, Time, Loc) :-
 
     jpl_array_to_list(Localization_Array, LocList),
 
-
     [M00, M01, M02, M03, M10, M11, M12, M13, M20, M21, M22, M23, M30, M31, M32, M33] = LocList,
 
-    atomic_list_concat(['rotMat3D_',M00,'_',M01,'_',M02,'_',M03,'_',M10,'_',M11,'_',M12,'_',M13,'_',M20,'_',M21,'_',M22'_',M23,'_',M30,'_',M31,'_',M32'_',M33], LocIdentifier),
+    atomic_list_concat(['rotMat3D_',M00,'_',M01,'_',M02,'_',M03,'_',M10,'_',M11,'_',M12,'_',M13,'_',M20,'_',M21,'_',M22,'_',M23,'_',M30,'_',M31,'_',M32,'_',M33], LocIdentifier),
 
     atom_concat('http://ias.cs.tum.edu/kb/knowrob.owl#', LocIdentifier, Loc),
     rdf_assert(Loc, rdf:type, knowrob:'RotationMatrix3D').
@@ -227,3 +259,16 @@ computable_perception_object_instances(Time, ObjectList) :-
     jpl_call(Client, 'getPerceptionObjects', [o1], Objects),
 
     jpl_array_to_list(Objects, ObjectList).
+
+failure_class(Error, Class) :-	
+	rdf_has(Error, rdf:type, Class),
+	rdf_individual_of(Class, modexecutiontrace:'CRAMFailure').
+
+failure_task(Error, Task) :-
+	task(Task),	
+	failure_class(Error, Class),
+	rdf_has(Task, modexecutiontrace:'eventFailure', Error).
+
+failure_attribute(Error,AttributeName,Value) :-
+	failure_class(Error, Class),
+	rdf_has(Error, AttributeName, Value).
