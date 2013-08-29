30f7d3fc437999314d4ce921b144e9593cb0c66b
Moritz Tenorth <tenorth@cs.uni-bremen.de>
2013-08-29 20:13:21 +0200

    added mng_transform_pose predicate, general refactoring and debugging

--- knowrob_mongo/prolog/knowrob_mongo.pl
+++ knowrob_mongo/prolog/knowrob_mongo.pl
@@ -17,10 +17,16 @@
 
 :- module(knowrob_mongo,
     [
-      mng_lookup_transform/4,
       mng_latest_designator_before_time/3,
-      mng_tf_pose_at_time/4,
-      mng_tf_pose/2
+      mng_designator_type/2,
+      mng_designator_props/3,
+      mng_obj_pose_by_desig/2,
+      
+      mng_lookup_transform/4,
+      mng_transform_pose/5,
+
+      mng_robot_pose/2,
+      mng_robot_pose_at_time/4
     ]).
 
 :- use_module(library('semweb/rdfs')).
@@ -49,6 +55,9 @@
 :- rdf_db:rdf_register_ns(srdl2comp, 'http://ias.cs.tum.edu/kb/srdl2-comp.owl#', [keep(true)]).
 
 
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
+% Designator integration
+% 
 
 %% mng_latest_designator_before_time(+TimePoint, -Type, -Pose) is nondet.
 %
@@ -72,6 +81,12 @@ mng_latest_designator_before_time(TimePoint, Type, PoseList) :-
   knowrob_coordinates:matrix4d_to_list(PoseMatrix4d, PoseList).
 
 
+% Computable: determine object pose based on designator
+mng_obj_pose_by_desig(Obj, Pose) :-
+
+  % TODO: avoid multiple creation of pose instances
+  rdf_has(Obj, knowrob:designator, Designator),
+  mng_designator_props(Designator, 'POSE', Pose).
 
 
 mng_designator_type(Designator, Type) :-
@@ -105,43 +120,52 @@ mng_desig_get_value(_Designator, DesigJava, Prop, Value) :-
   jpl_ref_to_type(ValIn,  class([org,knowrob,interfaces,mongo,types],['Designator'])),
   Value=ValIn. % TODO
 
+
 % create observation of the object to which the designator is attached
 mng_desig_get_value(Designator, DesigJava, Prop, Pose) :-
 
   jpl_call(DesigJava, 'get', [Prop], PoseStamped),
   jpl_ref_to_type(PoseStamped,  class([org,knowrob,interfaces,mongo,types],['PoseStamped'])),
 
-  jpl_call(PoseStamped, 'getMatrix4d', [], PoseMatrix4d),
-  knowrob_coordinates:matrix4d_to_list(PoseMatrix4d, PoseList),
-  create_pose(PoseList, Pose),
-
   % find out which object we are talking about
   rdf_has(Obj, knowrob:designator, Designator),
 
-  % determine detection type (e.g. perception)
-  jpl_call(DesigJava, 'getDetectionType', [], DetectionType),
-  
-  % create perception instance attached to the object this designator belongs to
-  create_perception_instance([DetectionType], Detection),
-  set_object_perception(Obj, Detection),
-  rdf_assert(Detection, knowrob:eventOccursAt, Pose),
-
-  % set time point for pose
+  % get pose time
   jpl_get(PoseStamped, 'header', Header),
   jpl_get(Header, 'stamp', Stamp),
   jpl_get(Stamp,  'secs', TimeSecs),
   term_to_atom(TimeSecs, TimeSecsAtom),
   atom_concat('http://ias.cs.tum.edu/kb/knowrob_mongo.owl#timepoint_', TimeSecsAtom, PoseTimePoint),
 
-  jpl_get(Header, 'frame_id', Frame),
-  rdf_assert(Pose, knowrob:relativeTo, Frame),
+  % transform into /map
+  jpl_call(PoseStamped, 'getMatrix4d', [], PoseMatrix4d),
+  jpl_get(Header, 'frame_id', SourceFrame),
+  knowrob_coordinates:matrix4d_to_list(PoseMatrix4d, PoseListIn),
+  mng_transform_pose(PoseListIn, SourceFrame, '/map', PoseTimePoint, PoseListOut),
+  create_pose(PoseListOut, Pose),
+
+  % determine detection type (e.g. perception)
+  jpl_call(DesigJava, 'getDetectionType', [], DetectionType),
   
+  % create perception instance attached to the object this designator belongs to
+  atom_concat('http://ias.cs.tum.edu/kb/knowrob.owl#', DetectionType, DClass),
+  rdf_instance_from_class(DClass, Detection),
+  set_object_perception(Obj, Detection),
+  rdf_assert(Detection, knowrob:eventOccursAt, Pose),
+
   rdf_assert(PoseTimePoint, rdf:type, 'http://ias.cs.tum.edu/kb/knowrob.owl#TimePoint'),
   rdf_assert(Detection, knowrob:startTime, PoseTimePoint).
 
 % just return the value for other properties
 mng_desig_get_value(_Designator, DesigJava, Prop, Value) :-
   jpl_call(DesigJava, 'get', [Prop], Value).
+
+
+
+
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
+% Tf integration
+%
 
 
 %% mng_lookup_transform(+Target, +Source, +TimePoint, -Transform) is nondet.
@@ -164,20 +188,45 @@ mng_lookup_transform(Target, Source, TimePoint, Transform) :-
   knowrob_coordinates:matrix4d_to_list(TransformMatrix4d, Transform).
 
 
-%% mng_tf_pose(+RobotPart, -Pose) is nondet.
+mng_transform_pose(PoseListIn, SourceFrame, TargetFrame, TimePoint, PoseListOut) :-
+
+  rdf_split_url(_, TimePointLocal, TimePoint),
+  atom_concat('timepoint_', TimeAtom, TimePointLocal),
+  term_to_atom(Time, TimeAtom),
+  TimeInt is round(Time),
+
+  knowrob_coordinates:list_to_matrix4d(PoseListIn, MatrixIn),
+  jpl_new('ros.communication.Time', [], TimeIn),
+  jpl_set(TimeIn, 'secs', TimeInt),
+  jpl_new('tfjava.Stamped', [MatrixIn, SourceFrame, TimeIn], StampedIn),
+
+  knowrob_coordinates:list_to_matrix4d([1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1], MatrixOut),
+  jpl_new('ros.communication.Time', [], TimeOut),
+  jpl_set(TimeOut, 'secs', TimeInt),
+  jpl_new('tfjava.Stamped', [MatrixOut, '/base_link', TimeOut], StampedOut),
+
+  jpl_new('org.knowrob.interfaces.mongo.MongoDBInterface', [], DB),
+  jpl_call(DB, 'transformPose', [TargetFrame, StampedIn, StampedOut], @(true)),
+
+  jpl_call(StampedOut, 'getData', [], MatrixOut2),
+  knowrob_coordinates:matrix4d_to_list(MatrixOut2, PoseListOut).
+
+
+
+%% mng_robot_pose(+RobotPart, -Pose) is nondet.
 %
 % Read the pose of RobotPart in /map coordinates from logged tf data, default to 'now'
 %
 % @param RobotPart  Instance of a robot part with the 'urdfName' property set
 % @param Pose       Instance of a knowrob:RotationMatrix3D with the pose data
 %
-mng_tf_pose(RobotPart, Pose) :-
+mng_robot_pose(RobotPart, Pose) :-
 
   get_timepoint(TimePoint),
-  mng_tf_pose_at_time(RobotPart, '/map', TimePoint, Pose).
+  mng_robot_pose_at_time(RobotPart, '/map', TimePoint, Pose).
 
 
-%% mng_tf_pose_at_time(+RobotPart, +TargetFrame, +TimePoint, -Pose) is nondet.
+%% mng_robot_pose_at_time(+RobotPart, +TargetFrame, +TimePoint, -Pose) is nondet.
 %
 % Read the pose of RobotPart in the given coordinate frame from logged tf data
 %
@@ -186,7 +235,7 @@ mng_tf_pose(RobotPart, Pose) :-
 % @param TimePoint    Instance of knowrob:TimePoint
 % @param Pose         Instance of a knowrob:RotationMatrix3D with the pose data
 %
-mng_tf_pose_at_time(RobotPart, TargetFrame, TimePoint, Pose) :-
+mng_robot_pose_at_time(RobotPart, TargetFrame, TimePoint, Pose) :-
 
   owl_has(RobotPart, 'http://ias.cs.tum.edu/kb/srdl2-comp.owl#urdfName', literal(SourceFrameID)),
   atom_concat('/', SourceFrameID, SourceFrame),
@@ -209,48 +258,31 @@ mng_tf_pose_at_time(RobotPart, TargetFrame, TimePoint, Pose) :-
 mng_obj_pose_at_time(Obj, SourceFrame, TargetFrame, TimePoint, Pose) :-
 
   % read object pose in original coordinates at TimePoint
-  (object_pose_at_time(Obj, Time, PoseListIn)
+  (object_pose_at_time(Obj, TimePoint, PoseListIn)
      -> true ;
         PoseListIn = [1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1]),
-
-  rdf_split_url(_, TimePointLocal, TimePoint),
-  atom_concat('timepoint_', TimeAtom, TimePointLocal),
-  term_to_atom(Time, TimeAtom),
-  TimeInt is round(Time),
         
-  knowrob_coordinates:list_to_matrix4d(PoseListIn, MatrixIn),
-  jpl_new('ros.communication.Time', [], TimeIn),
-  jpl_set(TimeIn, 'secs', TimeInt),
-  jpl_new('tfjava.Stamped', [MatrixIn, SourceFrame, TimeIn], StampedIn),
-
-  knowrob_coordinates:list_to_matrix4d([1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1], MatrixOut),
-  jpl_new('ros.communication.Time', [], TimeOut),
-  jpl_set(TimeOut, 'secs', TimeInt),
-  jpl_new('tfjava.Stamped', [MatrixOut, '/base_link', TimeOut], StampedOut),
-
-  jpl_new('org.knowrob.interfaces.mongo.MongoDBInterface', [], DB),
-  jpl_call(DB, 'transformPose', [TargetFrame, StampedIn, StampedOut], @(true)),
-
-  jpl_call(StampedOut, 'getData', [], MatrixOut2),
-  knowrob_coordinates:matrix4d_to_list(MatrixOut2, PoseList),
-  create_pose(PoseList, Pose),
+  mng_transform_pose(PoseListIn, SourceFrame, TargetFrame, TimePoint, PoseListOut),
+  create_pose(PoseListOut, Pose),
   rdf_assert(Pose, knowrob:tfFrame, TargetFrame),
 
-  % TODO: set previousDetectionOfObject / latestDetectionOfObject
   create_perception_instance(['Proprioception'], Perception),
   set_object_perception(Obj, Perception),
   rdf_assert(Perception, knowrob:eventOccursAt, Pose),
 
-  % set time point for pose
-  jpl_get(TimeOut, 'secs', PoseTime),
-  term_to_atom(PoseTime, PoseTimeAtom),
-  atom_concat('http://ias.cs.tum.edu/kb/knowrob_mongo.owl#timepoint_', PoseTimeAtom, PoseTimePoint),
-
-  rdf_assert(PoseTimePoint, rdf:type, 'http://ias.cs.tum.edu/kb/knowrob.owl#TimePoint'),
-  rdf_assert(Perception, knowrob:startTime, PoseTimePoint).
+  % set time point for pose,
+  rdf_assert(Perception, knowrob:startTime, TimePoint).
 
 
 
+
+
+
+
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
+% Higher-level reasoning methods
+%
+  
 %% obj_visible_in_camera(+Obj, ?Camera, +TimePoint) is nondet.
 %
 % Check if Obj is visible by Camera at time TimePoint.
@@ -277,11 +309,13 @@ obj_visible_in_camera(Obj, Camera, TimePoint) :-
   once(owl_has(Camera, 'http://ias.cs.tum.edu/kb/srdl2-comp.owl#urdfName', literal(CamFrameID))),
   atom_concat('/', CamFrameID, CamFrame),
 
-  mng_tf_pose_at_time(Obj, CamFrame, TimePoint, RelObjPose),
+  owl_has(Obj, 'http://ias.cs.tum.edu/kb/srdl2-comp.owl#urdfName', literal(ObjFrameID)),
+  atom_concat('/', ObjFrameID, ObjFrame),
+  
+  mng_transform_pose([1,0,0,0,  0,1,0,0,  0,0,1,0,  0,0,0,1], ObjFrame,
+                    CamFrame, TimePoint, RelObjPose),
 
-  owl_has(RelObjPose, knowrob:m03, literal(type(_,ObjX))),
-  owl_has(RelObjPose, knowrob:m13, literal(type(_,ObjY))),
-  owl_has(RelObjPose, knowrob:m23, literal(type(_,ObjZ))),
+  RelObjPose = [_,_,_,ObjX,_,_,_,ObjY,_,_,_,ObjZ,_,_,_,_],
 
   BearingX is atan2(ObjY, ObjX),
   BearingY is atan2(ObjZ, ObjX),
@@ -293,32 +327,47 @@ obj_visible_in_camera(Obj, Camera, TimePoint) :-
 
 
   
-obj_blocked_by_in_camera(Obj, Blocker, Camera, TimePoint) :-
+obj_blocked_by_in_camera(Obj, Blocker, Camera, TimePoint, ObjXDeg, ObjYDeg, BlkXDeg, BlkYDeg) :-
 
   findall(Camera, owl_individual_of(Camera, srdl2comp:'Camera'), Cameras),
   member(Camera, Cameras),
 
-  % Read object pose w.r.t. camera
+  % Read camera frame ID
   once(owl_has(Camera, 'http://ias.cs.tum.edu/kb/srdl2-comp.owl#urdfName', literal(CamFrameID))),
   atom_concat('/', CamFrameID, CamFrame),
 
-  mng_tf_pose_at_time(Obj,     CamFrame, TimePoint, RelObjPose),
-  owl_has(RelObjPose, knowrob:m03, literal(type(_,ObjX))),
-  owl_has(RelObjPose, knowrob:m13, literal(type(_,ObjY))),
-  owl_has(RelObjPose, knowrob:m23, literal(type(_,ObjZ))),
-
+  
+  % Read object pose w.r.t. camera
+  object_pose_at_time(Obj, TimePoint, PoseListObj),
+  mng_transform_pose(PoseListObj, '/map', CamFrame, TimePoint, ObjPoseInCamFrame),
+  ObjPoseInCamFrame = [_,_,_,ObjX,_,_,_,ObjY,_,_,_,ObjZ,_,_,_,_],
   ObjBearingX is atan2(ObjY, ObjX),
   ObjBearingY is atan2(ObjZ, ObjX),
 
-  mng_tf_pose_at_time(Blocker, CamFrame, TimePoint, RelBlkPose),
-  owl_has(RelBlkPose, knowrob:m03, literal(type(_,BlkX))),
-  owl_has(RelBlkPose, knowrob:m13, literal(type(_,BlkY))),
-  owl_has(RelBlkPose, knowrob:m23, literal(type(_,BlkZ))),
+  % debug
+  ObjXDeg is ObjBearingX /2 /pi * 360,
+  ObjYDeg is ObjBearingY /2 /pi * 360,
+  
+  % Read poses of blocking robot parts w.r.t. camera
+  sub_component('http://ias.cs.tum.edu/kb/PR2.owl#PR2Robot1', Blocker),
+  rdfs_individual_of(Blocker, 'http://ias.cs.tum.edu/kb/srdl2-comp.owl#UrdfLink'),
+  owl_has(Blocker, 'http://ias.cs.tum.edu/kb/srdl2-comp.owl#urdfName', literal(PartFrameID)),
+  atom_concat('/', PartFrameID, PartFrame),
 
+  print(PartFrame),
+  % transform identity pose from robot part frame to camera frame
+  mng_transform_pose([1,0,0,0,  0,1,0,0,  0,0,1,0,  0,0,0,1], PartFrame,
+                     CamFrame, TimePoint, BlockerPoseInCamFrame),
+  BlockerPoseInCamFrame = [_,_,_,BlkX,_,_,_,BlkY,_,_,_,BlkZ,_,_,_,_],
   BlkBearingX is atan2(BlkY, BlkX),
   BlkBearingY is atan2(BlkZ, BlkX),
 
-  abs(ObjBearingX - BlkBearingX) < 10/360*pi,
-  abs(ObjBearingY - BlkBearingY) < 10/360*pi.
+  % debug
+  BlkXDeg is BlkBearingX /2 /pi * 360,
+  BlkYDeg is BlkBearingY /2 /pi * 360.
+%   ,
+%   
+%   abs(ObjBearingX - BlkBearingX) < 10/360 * 2 * pi,
+%   abs(ObjBearingY - BlkBearingY) < 10/360 * 2 * pi.
 
 
