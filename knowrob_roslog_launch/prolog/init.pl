5bf27e19087b1a31aee3dfa15585054f9700ac90
Moritz Tenorth <tenorth@cs.uni-bremen.de>
2014-02-21 17:09:28 +0100

    added package with launch file for starting KnowRob with semantic map, visualization, logging etc

---
+++ knowrob_roslog_launch/prolog/init.pl
@@ -0,0 +1,9 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% dependencies
+
+
+:- register_ros_package(knowrob_roslog_launch).
+:- register_ros_package(mod_execution_trace).
+:- register_ros_package(knowrob_vis).
+:- register_ros_package(iai_maps).
+
