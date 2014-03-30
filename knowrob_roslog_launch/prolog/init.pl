f73ee19e6431b7dcf58a010ffb8b72936b6885c9
Asil Kaan Bozcuoğlu <asil@cs.uni-bremen.de>
2014-03-30 23:59:49 +0200

    added init queries to init.pl

--- knowrob_roslog_launch/prolog/init.pl
+++ knowrob_roslog_launch/prolog/init.pl
@@ -6,4 +6,9 @@
 :- register_ros_package(mod_execution_trace).
 :- register_ros_package(knowrob_vis).
 :- register_ros_package(iai_maps).
+:- register_ros_package(mod_srdl).
+:- owl_parse('/home/demo/work/ros/groovy/rosbuild_ws/knowrob/ias_semantic_map/owl/ias_semantic_map.owl', false, false, true).
+:- owl_parse('/var/roslog/exp-2014-03-24_13-18-34/cram_log.owl', false, false, true).
+:- visualisation_canvas.
+:- diagram_canvas.
 
