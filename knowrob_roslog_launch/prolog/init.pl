c6d18a085bb0b4ae035bd060e08ce268b12245e0
Moritz Tenorth <tenorth@cs.uni-bremen.de>
2014-04-01 15:21:50 +0200

    add knowrob_motion_constraints to demo launch file

--- knowrob_roslog_launch/prolog/init.pl
+++ knowrob_roslog_launch/prolog/init.pl
@@ -7,8 +7,8 @@
 :- register_ros_package(knowrob_vis).
 :- register_ros_package(iai_maps).
 :- register_ros_package(mod_srdl).
-:- owl_parse('/home/demo/work/ros/groovy/rosbuild_ws/knowrob/ias_semantic_map/owl/ias_semantic_map.owl', false, false, true).
-:- owl_parse('/var/roslog/exp-2014-03-24_13-18-34/cram_log.owl', false, false, true).
+:- register_ros_package(knowrob_motion_constraints).
+
 :- visualisation_canvas.
 :- diagram_canvas.
 
