b28b425325f2e2b2c4a21df8909ad724ee144d9f
Moritz Tenorth <tenorth@cs.uni-bremen.de>
2014-07-24 15:43:09 +0200

    fixed knowrob_cram

--- knowrob_cram/prolog/init.pl
+++ knowrob_cram/prolog/init.pl
@@ -15,10 +15,10 @@
 %% along with this program.  If not, see <http://www.gnu.org/licenses/>.
 %%
 
-:- register_ros_package(ias_knowledge_base).
+:- register_ros_package(knowrob_common).
 :- register_ros_package(knowrob_mongo).
 
 :- register_ros_package(knowrob_cram).
 :- use_module(library('knowrob_cram')).
 
-:- owl_parser:owl_parse('package://knowrob_cram/owl/knowrob_cram.owl', false, false, true).
+:- owl_parser:owl_parse('package://knowrob_cram/owl/knowrob_cram.owl').
