9fc72c60d1f182acd9582a687b2a3e2c28011434
Daniel Beßler <danielb@informatik.uni-bremen.de>
2015-01-27 17:01:01 +0100

    Adding SAPHARI knowrob project that defines some project specific properties and classes.

---
+++ knowrob_saphari/prolog/knowrob_saphari.pl
@@ -0,0 +1,41 @@
+
+/** <module> knowrob_saphari
+
+  Copyright (C) 2015 by Daniel Beßler
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+@author Daniel Beßler
+@license GPL
+*/
+
+
+:- module(knowrob_saphari,
+    [
+    ]).
+:- use_module(library('semweb/rdf_db')).
+:- use_module(library('semweb/rdfs')).
+:- use_module(library('owl')).
+:- use_module(library('rdfs_computable')).
+:- use_module(library('owl_parser')).
+:- use_module(library('comp_temporal')).
+:- use_module(library('knowrob_mongo')).
+
+:- rdf_db:rdf_register_ns(knowrob, 'http://knowrob.org/kb/knowrob.owl#',  [keep(true)]).
+:- rdf_db:rdf_register_ns(knowrob_saphari, 'http://knowrob.org/kb/knowrob_saphari.owl#', [keep(true)]).
+:- rdf_db:rdf_register_ns(knowrob_cram, 'http://knowrob.org/kb/knowrob_cram.owl#', [keep(true)]).
+
+% define predicates as rdf_meta predicates
+% (i.e. rdf namespaces are automatically expanded)
+:-  rdf_meta.
