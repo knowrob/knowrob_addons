25283178b0fb2260a62cd9e2aa5f8fef9fa82477
zyfang <yzfang1@gmail.com>
2015-11-26 16:11:08 +0100

    added title to timeline query

--- knowrob_sim/prolog/knowrob_sim.pl
+++ knowrob_sim/prolog/knowrob_sim.pl
@@ -59,7 +59,7 @@
         anyact/3,
         intersected_uid_event/6,
         sim_subsumes/4,
-        sim_timeline_val/4
+        sim_timeline_val/5
     ]).
 :- use_module(library('semweb/rdf_db')).
 :- use_module(library('semweb/rdfs')).
@@ -103,7 +103,7 @@
     visualize_simulation_particles(+,+,+,r),
     anyact(r,r,r),
     sim_subsumes(r,r,r,r),
-    sim_timeline_val(r,r,r,r),
+    sim_timeline_val(r,r,r,r,r),
     successful_simacts_for_goal(+,-).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -235,9 +235,11 @@ simact_contact_specific(Experiment, Event, ObjectInstance1, ObjectInstance2) :-
 %% Function returns a range for each event in the list during which that event is true
 %
 % Example call for plotting timeline: sim_timeline_val(Exp, Events, Times), add_diagram('id', 'Title', timeline, 'Time', 'Events', 300, 300, '12px', [[Events,Times]]).
-sim_timeline_val(Expname, EventNamesList, StartTimeList, EndTimeList):-
+sim_timeline_val(Expname, EventNamesList, StartTimeList, EndTimeList, Title):-
     %Find all events from a single experiment
     rdf_has(_, knowrob:'experiment', literal(type(_, Expname))),
+    %Get title for chart
+    string_concat(Expname, ' Timeline', Title),
     %Get list of all the events that happen in the experiment for extracting timepoints
     findall(Type, simact(Expname,Type), EventList), 
     %Get list of event names that happened in the experment for putting in the timeline
