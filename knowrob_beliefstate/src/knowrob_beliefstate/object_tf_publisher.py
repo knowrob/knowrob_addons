e507c770285f97ffbe964635087b9556a5da4c0d
ichumuh <stelter@tzi.de>
2017-06-15 15:50:11 +0200

    object tf frame publisher almost done

--- knowrob_beliefstate/src/knowrob_beliefstate/object_tf_publisher.py
+++ knowrob_beliefstate/src/knowrob_beliefstate/object_tf_publisher.py
@@ -2,82 +2,111 @@
 import rospy
 import tf
 from collections import defaultdict
+from knowrob_beliefstate.srv._DirtyObject import DirtyObject, DirtyObjectResponse
+from std_msgs.msg._ColorRGBA import ColorRGBA
 from visualization_msgs.msg._Marker import Marker
 from json_prolog import json_prolog
+
 
 class ThorinObject(object):
     def __init__(self):
         self.transform = None
         self.mesh_path = ''
+        self.color = ColorRGBA()
 
-    def add_transform(self, transform):
-        self.transform = transform
-        self.object_name = str(transform[1]).split('#')[1]
+    def update_color(self, r, g, b, a):
+        self.color = ColorRGBA()
+        self.color.r = float(r)
+        self.color.g = float(g)
+        self.color.b = float(b)
+        self.color.a = float(a)
+
+    def update_transform(self, ref_frame, object_name, translation, rotation):
+        self.transform = [ref_frame, object_name, translation, rotation]
+        self.object_name = str(object_name)
 
     def get_marker(self):
         marker = Marker()
         marker.header.frame_id = self.object_name
         marker.type = marker.MESH_RESOURCE
         marker.action = Marker.ADD
-        marker.id = 0
-        marker.ns = 'muh'
+        marker.id = 1337
+        marker.ns = self.get_short_name()
+        marker.color = self.color
+        marker.scale.x = 1
+        marker.scale.y = 1
+        marker.scale.z = 1
         marker.pose.orientation.w = 1
-        marker.mesh_resource = self.mesh_path
+        marker.mesh_resource = self.mesh_path[:-4] + '.dae'
         return marker
 
+    def get_short_name(self):
+        return self.object_name.split('#')[1]
+
+
 class ObjectTfPublisher(object):
-    def __init__(self):
+    def __init__(self, tf_frequency):
+        self.tf_frequency = tf_frequency
         self.prolog = json_prolog.Prolog()
         self.tf_broadcaster = tf.TransformBroadcaster()
         self.marker_publisher = rospy.Publisher('/visualization_marker', Marker, queue_size=10)
+        self.dirty_object_srv = rospy.Service('~dirty_object', DirtyObject, self.dirty_cb)
         self.objects = defaultdict(lambda: ThorinObject())
 
-    def load_object_ids(self):
-        query = self.prolog.query('get_known_object_ids(A)')
-        solutions = [x for x in query.solutions()]
-        if len(solutions) > 1:
-            rospy.logwarn('get_known_object_ids returned more than one list')
-        for object_id in solutions[0]['A']:
-            self.objects[object_id] = ThorinObject()
+    def dirty_cb(self, srv_msg):
+        self.load_object(srv_msg.object_id)
+        self.publish_object_frames()
+        rospy.loginfo('updated {}'.format(srv_msg.object_id))
+        r = DirtyObjectResponse()
+        r.success = True
+        return r
 
-        rospy.loginfo('Loaded object ids {}'.format(self.objects))
-        query.finish()
-
-    def get_object_transform(self, object_id):
-        # q = "get_object_transform('{}', B)".format(object_id)
-        # q = "get_object_mesh_path('{}', B)".format(object_id)
-        # q = "get_associated_transform(_, _, _, TempRei, _, 'http://knowrob.org/kb/knowrob_paramserver.owl#hasTransform', Transform)."
-        # q = "rdf_has(TempExt, paramserver:'endsAtTime', B)"
-        # q = "rdf_has(TempRei, paramserver:'temporalExtent', TempExt)"
-        # q = "rdf_has('{}', paramserver:'hasTransform', TempRei)".format(object_id)
-        # q = "rdf_has('{}', paramserver:'hasTransform', TempRei)".format(object_id)
-        # q = "rdf_has('{}', paramserver:'hasTransform', TempRei)," \
-        #     "rdf_has(TempRei, paramserver:'temporalExtent', TempExt)".format(object_id)
-
-        # print(q)
-        # query = self.prolog.query(q)
-        # solutions = [x for x in query.solutions()]
-        # print(solutions)
-        # for s in query.solutions():
-        #     print(s)
-        # query.finish()
-        return ['map', object_id, (0, 0, 0), (0, 0, 0, 1)]
-
-    def get_object_mash(self, object_id):
-        q = "get_object_mesh_path('{}', B)".format(object_id)
+    def prolog_query(self, q):
         query = self.prolog.query(q)
         solutions = [x for x in query.solutions()]
         if len(solutions) > 1:
-            rospy.logwarn('get_known_object_ids returned more than one list')
-        self.objects[object_id].mesh_path = str(solutions[0]['B'])
+            rospy.logwarn('{} returned more than one result'.format(q))
+        elif len(solutions) == 0:
+            rospy.logwarn('{} returned nothing'.format(q))
         query.finish()
-        # marker = Marker()
-        # marker.header.frame_id = self.objects[object_id]['transform']
-        # marker.type = marker.MESH_RESOURCE
+        return solutions
 
-    def load_object_transforms(self):
+    def load_objects(self):
+        self.load_object_ids()
         for object_id in self.objects.keys():
-            self.objects[object_id].add_transform(self.get_object_transform(object_id))
+            self.load_object(object_id)
+
+    def load_object(self, object_id):
+        self.load_object_transform(object_id)
+        self.load_object_mesh(object_id)
+        self.load_object_color(object_id)
+
+    def load_object_ids(self):
+        q = 'get_known_object_ids(A)'
+        solutions = self.prolog_query(q)
+        for object_id in solutions[0]['A']:
+            self.objects[object_id] = ThorinObject()
+        rospy.loginfo('Loaded object ids {}'.format(self.objects.keys()))
+
+    def load_object_color(self, object_id):
+        q = "get_object_color('{}', A)".format(object_id)
+        solutions = self.prolog_query(q)
+        self.objects[object_id].update_color(*solutions[0]['A'])
+
+    def load_object_transform(self, object_id):
+        # q = "get_object_transform('{}', A)".format(object_id)
+        # solutions = self.prolog_query(q)
+        # self.objects[object_id].update_transform('left_gripper_tool_frame', object_id, (0, 0, 0), (0, 0, 0, 1))
+        self.objects[object_id].update_transform('map', object_id, (0, 0, 0), (0, 0, 0, 1))
+
+    def load_object_mesh(self, object_id):
+        q = "get_object_mesh_path('{}', A)".format(object_id)
+        solutions = self.prolog_query(q)
+        self.objects[object_id].mesh_path = str(solutions[0]['A'])
+
+    def publish_object_markers(self):
+        for object_id, v in self.objects.items():
+            self.marker_publisher.publish(v.get_marker())
 
     def publish_object_frames(self):
         for object_id, thorin_object in self.objects.items():
@@ -88,20 +117,9 @@ class ObjectTfPublisher(object):
                                               thorin_object.object_name,
                                               ref_frame)
 
-    def load_object_shapes(self):
-        for object_id in self.objects.keys():
-            self.get_object_mash(object_id)
-
-    def publish_object_markers(self):
-        for object_id, v in self.objects.items():
-            print('{} {}'.format(object_id, v.mesh_path))
-            self.marker_publisher.publish(v.get_marker())
-
     def loop(self):
-        self.load_object_ids()
-        self.load_object_transforms()
-        self.load_object_shapes()
-        rate = rospy.Rate(1)
+        self.load_objects()
+        rate = rospy.Rate(self.tf_frequency)
         while not rospy.is_shutdown():
             self.publish_object_frames()
             self.publish_object_markers()
@@ -110,5 +128,5 @@ class ObjectTfPublisher(object):
 
 if __name__ == '__main__':
     rospy.init_node('object_tf_publisher')
-    asdf = ObjectTfPublisher()
+    asdf = ObjectTfPublisher(1)
     asdf.loop()
