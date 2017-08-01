e2d25599b88cc2e97ef70b2246df0b13f1073639
Asil Kaan Bozcuoglu <asil@cs.uni-bremen.de>
2017-08-01 16:38:27 +0900

    handle differently for each atomic types in json

--- knowrob_cloud_logger/prolog/knowrob_cloud_logger.pl
+++ knowrob_cloud_logger/prolog/knowrob_cloud_logger.pl
@@ -134,6 +134,12 @@ read_next_prolog_query(Result) :-
     cloud_interface(CL),
     jpl_call(CL, 'readPrologNextSolution', [], Result).
 
-read_next_prolog_query(Field, Result) :-
+read_next_prolog_query(Field, R) :-
     cloud_interface(CL),
-    jpl_call(CL, 'readPrologNextSolution', [Field], Result).
+    jpl_call(CL, 'readPrologNextSolution', [Field], J),
+    jpl_object_to_class(J, C),
+    ((atom(J), R=J);
+     (jpl_object_to_class(J, C), jpl_class_to_classname(C, 'java.lang.Float'), jpl_call(J,'doubleValue',[],R));
+     (jpl_object_to_class(J, C), jpl_class_to_classname(C, 'java.lang.Double'), jpl_call(J,'doubleValue',[],R));
+     (jpl_object_to_class(J, C), jpl_class_to_classname(C, 'java.lang.Integer'), jpl_call(J,'intValue',[],R));
+     (jpl_object_to_class(J, C), jpl_class_to_classname(C, 'java.lang.Boolean'), jpl_call(J,'booleanValue',[],R))).
