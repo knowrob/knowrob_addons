24938399944029aca23d00e34bc6f101ec3f3936
Daniel Beßler <danielb@cs.uni-bremen.de>
2017-08-25 10:08:38 +0200

    initialiazation for battat assembly

--- knowrob_assembly/prolog/knowrob_battat.pl
+++ knowrob_assembly/prolog/knowrob_battat.pl
@@ -1,6 +1,20 @@
 
-% Just an empty file for now
-
 :- module(knowrob_battat,
     [
+        battat_initialize/0,
+        battat_initialize_sim/0
     ]).
+
+:- use_module(library('semweb/rdf_db')).
+:- use_module(library('semweb/rdfs')).
+:- use_module(library('owl')).
+:- use_module(library('owl_parser')).
+
+battat_initialize :-
+  owl_parser:owl_parse('package://knowrob_assembly/owl/battat_toys.owl'),
+  owl_parser:owl_parse('package://knowrob_assembly/owl/battat_airplane_test.owl').
+
+battat_initialize_sim :-
+  battat_initialize,
+  owl_parser:owl_parse('package://knowrob_assembly/owl/battat_airplane_simulation.owl').
+
