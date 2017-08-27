f3e2e8c96b2aaf4d389c5c81cb8d7b18d239d30f
Daniel Beßler <danielb@cs.uni-bremen.de>
2017-08-27 23:48:54 +0200

    +assemblage_create_connection +assemblage_connection_established

--- knowrob_assembly/prolog/knowrob_assembly.pl
+++ knowrob_assembly/prolog/knowrob_assembly.pl
@@ -5,13 +5,17 @@
         assemblage_underspecified/1,
         assemblage_parent/2,
         assemblage_established/1,
+        assemblage_connection_established/1,
         assemblage_destroy/1,
         assemblage_graspable_part/2,
+        assemblage_possible_grasp/3,
+        assemblage_possible_grasp/2,
         assemblage_fixed_part/1,
         assemblage_part/2,
         assemblage_part_links_fixtures/2,
         assemblage_part_links_part/2,
-        assemblage_part_blocked_affordance/2
+        assemblage_part_blocked_affordance/2,
+        assemblage_create_connection/3
     ]).
 
 :- use_module(library('semweb/rdf_db')).
@@ -31,13 +35,17 @@
       assemblage_underspecified(r),
       assemblage_parent(r,r),
       assemblage_established(r),
+      assemblage_connection_established(r),
       assemblage_destroy(r),
       assemblage_graspable_part(r,r),
+      assemblage_possible_grasp(r,r,t),
+      assemblage_possible_grasp(r,t),
       assemblage_fixed_part(r),
       assemblage_part(r,r),
       assemblage_part_blocked_affordance(r,r),
       assemblage_part_links_fixtures(r,t),
-      assemblage_part_links_part(r,t).
+      assemblage_part_links_part(r,t),
+      assemblage_create_connection(r,t,-).
 
 %% assemblage_specified(+Assemblage) is det.
 %
@@ -74,6 +82,13 @@ assemblage_relevant_restriction_(P) :-
 assemblage_established(Assemblage) :-
   rdf_has(Assemblage, knowrob_assembly:'assemblageEstablished', literal(type(_,'true'))).
 
+%% assemblage_connection_established(?Connection) is det.
+%
+assemblage_connection_established(Connection) :-
+  rdf_has(Assemblage, knowrob_assembly:'usesConnection', Connection), !,
+  rdf_has(Assemblage, knowrob_assembly:'assemblageEstablished', literal(type(_,'true'))), !.
+assemblage_connection_established(_Connection).
+
 %% assemblage_parent(?Assemblage,?ParentAssemblage) is det.
 %
 assemblage_parent(Assemblage, ParentAssemblage) :-
@@ -81,9 +96,10 @@ assemblage_parent(Assemblage, ParentAssemblage) :-
 
 %% subassemblage(+Assemblage, ?ChildAssemblage) is det.
 %
-% FIXME: this shows a weakness of the model, the hierarchy can be interpreted in two directions:
+% TODO: this shows a weakness of the model, the hierarchy can be interpreted in two directions:
 %             A linksAssemblage B always implies B linksAssemblage A.
 %        thus I need to check on restrictions here instead of using just linksAssemblage property.
+%
 subassemblage(Assemblage, ChildAssemblage) :-
   ground(Assemblage), !,
   assemblage_linksAssemblage(Assemblage, ChildAssemblage),
@@ -123,8 +139,45 @@ assemblage_linksAssemblage_restriction(Assemblage, ChildAssemblageType) :-
 
 %% assemblage_graspable_part(+Assemblage,+Part) is det.
 %
-assemblage_graspable_part(_Assemblage, _Part) :-
-  true. % TODO: check if grasp affordance available that does not block any affordance required by assemblage
+assemblage_graspable_part(Assemblage, Part) :-
+  rdf_has(Assemblage, knowrob_assembly:'usesConnection', Conn),
+  assemblage_part(Assemblage, Part),
+  assemblage_possible_grasp(Part, Conn, _).
+
+%% assemblage_possible_grasp(+Assemblage, -(GraspObj,GraspAff,GraspSpec)) is det.
+%% assemblage_possible_grasp(+Object, +TargetConnection, -(GraspObj,GraspAff,GraspSpec)) is det.
+%
+assemblage_possible_grasp(Assemblage, PossibleGrasp) :-
+  rdf_has(Assemblage, knowrob_assembly:'usesConnection', TargetConnection),
+  assemblage_part(Assemblage, Part),
+  assemblage_possible_grasp(Part, TargetConnection, PossibleGrasp).
+assemblage_possible_grasp(Object, TargetConnection, (GraspObj,GraspAff,GraspSpec)) :-
+  (GraspObj=Object ; assemblage_part_links_part(Object, GraspObj)),
+  rdf_has(GraspObj, knowrob_assembly:'hasAffordance', GraspAff),
+  rdfs_individual_of(GraspAff, knowrob_assembly:'GraspingAffordance'),
+  % ensure grasped affordance not used in (not yet established) target assemblage
+  \+ rdf_has(TargetConnection, knowrob_assembly:'consumesAffordance', GraspAff),
+  % ensure the affordance is not yet blocked
+  \+ assemblage_part_blocked_affordance(GraspObj, GraspAff),
+  % find the grasp specification
+  owl_has(GraspAff, knowrob_assembly:'graspAt', GraspSpec),
+  % TODO: also ensure that grasp does not indirectly block affordances required by TargetConnection
+  true.
+
+%% assemblage_create_connection(+ConnType,+Objects,-ConnId) is det.
+%
+assemblage_create_connection(ConnType, Objects, ConnId) :-
+  rdf_instance_from_class(ConnType, ConnId),
+  forall((
+    rdfs_subclass_of(ConnType,Restr),
+    rdfs_individual_of(Restr,owl:'Restriction'),
+    rdf_has(Restr, owl:onProperty, knowrob_assembly:'consumesAffordance'),
+    rdf_has(Restr, owl:'onClass', AffType)),once((
+      member(Obj,Objects),
+      rdf_has(Obj, knowrob_assembly:'hasAffordance', Affordance),
+      rdfs_individual_of(Affordance, AffType),
+      rdf_assert(ConnId, knowrob_assembly:'consumesAffordance', Affordance)
+    ))).
 
 %% assemblage_fixed_part(+FixedPart) is det.
 %
