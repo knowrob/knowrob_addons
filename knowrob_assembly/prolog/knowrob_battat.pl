90e1223243ab89c6f591aa60a315da91a6687e1a
Daniel Beßler <danielb@cs.uni-bremen.de>
2017-09-03 13:12:25 +0200

    removed invalid connection assertion

--- knowrob_assembly/prolog/knowrob_battat.pl
+++ knowrob_assembly/prolog/knowrob_battat.pl
@@ -17,7 +17,8 @@
 :- rdf_db:rdf_register_prefix(battat_sim, 'http://knowrob.org/kb/battat_airplane_simulation.owl#', [keep(true)]).
 
 :-  rdf_meta
-      battat_sim_plane_connection(r,r,t).
+      battat_sim_plane_connection(r,r,t),
+      battat_sim_plane_connection(r,r,t,r).
 
 battat_initialize :-
   owl_parser:owl_parse('package://knowrob_assembly/owl/battat_toys.owl'),
@@ -30,7 +31,7 @@ battat_initialize_sim :-
 
 battat_sim_plane_complete :-
   battat_sim_plane_connection(battat_toys:'BottomWingSlideInChassis', battat_sim:'PlaneBottomWing_1',
-      [battat_sim:'PlaneChassis_1',battat_sim:'PlaneBottomWing_1']),
+     [battat_sim:'PlaneChassis_1',battat_sim:'PlaneBottomWing_1']),
   battat_sim_plane_connection(battat_toys:'UnderBodySlideInFrame', battat_sim:'PlaneUnderBody_1',
       [battat_sim:'PlaneUnderBody_1',battat_sim:'PlaneChassis_1',battat_sim:'PlaneRearWing_1',battat_sim:'PlaneBottomWing_1']),
   battat_sim_plane_connection(battat_toys:'MotorGrillSlideInUnderBody', battat_sim:'PlaneMotorGrill_1',
@@ -48,14 +49,12 @@ battat_sim_plane_complete :-
       [battat_sim:'PlaneChassis_1',battat_sim:'PlaneFrontWheel_1']),
   battat_sim_plane_connection(battat_toys:'FrontWheelRightSlideInChassis', battat_sim:'PlaneFrontWheel_2',
       [battat_sim:'PlaneChassis_1',battat_sim:'PlaneFrontWheel_2']),
-  %%% Nuts
+  %% Nuts
   battat_sim_plane_connection(battat_toys:'PlaneFrontWheelLeftScrewedOnChassis', battat_sim:'BattatNut_1',
       [battat_sim:'PlaneChassis_1',battat_sim:'BattatNut_1']),
   battat_sim_plane_connection(battat_toys:'PlaneFrontWheelLeftScrewedOnChassis', battat_sim:'BattatNut_2',
       [battat_sim:'PlaneChassis_1',battat_sim:'BattatNut_2']),
-  %%% Bolts
-  battat_sim_plane_connection(battat_toys:'FrontWingsScrewedOnBody', battat_sim:'BattatBolt_2',
-      [battat_sim:'PlaneUpperBody_1',battat_sim:'PlaneTopWing_1',battat_sim:'BattatBolt_2']),
+  %% Bolts
   battat_sim_plane_connection(battat_toys:'FrontWingsScrewedOnBody', battat_sim:'BattatBolt_2',
       [battat_sim:'PlaneUpperBody_1',battat_sim:'PlaneTopWing_1',battat_sim:'BattatBolt_2']),
   battat_sim_plane_connection(battat_toys:'PlaneWindowScrewedOnBody', battat_sim:'BattatBolt_3',
@@ -65,7 +64,12 @@ battat_sim_plane_complete :-
   battat_sim_plane_connection(battat_toys:'RearWingScrewedOnBody', battat_sim:'BattatBolt_5',
       [battat_sim:'PlaneUpperBody_1',battat_sim:'PlaneRearWing_1',battat_sim:'BattatBolt_5']).
 
-battat_sim_plane_connection(ConnType, Primary, Parts) :-
-  assemblage_create_connection(ConnType, Parts, Conn),
-  % TODO: talk to simon why i have to wait here
-  cram_assembly_apply_connection(Primary, Conn).
+battat_sim_plane_connection(ConnType, Primary, Parts) :- battat_sim_plane_connection(ConnType, Primary, Parts, _).
+battat_sim_plane_connection(ConnType, Primary, Parts, Conn) :-
+  assemblage_connection_create(ConnType, Parts, Conn),
+  write('  connection '), rdf_write_readable(Conn), nl,
+  write('    primary: '), rdf_write_readable(Primary), nl,
+  write('    parts:   '), rdf_write_readable(Parts), nl,
+  forall( rdf_has(Conn, knowrob_assembly:'blocksAffordance', Aff), (
+          write('    blocksAffordance '), rdf_write_readable(Aff), nl )),
+  cram_assembly:cram_assembly_apply_connection(Primary, Conn).
