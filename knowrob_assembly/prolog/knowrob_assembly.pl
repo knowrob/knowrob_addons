405d19670dd368ac1bf53fcb82439455ecac20c2
Sascha Jongebloed <sasjonge@uni-bremen.de>
2018-01-16 20:25:41 +0100

    Added srdl2comp as namespace

--- knowrob_assembly/prolog/knowrob_assembly.pl
+++ knowrob_assembly/prolog/knowrob_assembly.pl
@@ -67,6 +67,7 @@
 :- rdf_db:rdf_register_ns(knowrob, 'http://knowrob.org/kb/knowrob.owl#', [keep(true)]).
 :- rdf_db:rdf_register_ns(knowrob_planning, 'http://knowrob.org/kb/knowrob_planning.owl#', [keep(true)]).
 :- rdf_db:rdf_register_ns(knowrob_assembly, 'http://knowrob.org/kb/knowrob_assembly.owl#', [keep(true)]).
+:- rdf_db:rdf_register_ns(srdl2comp, 'http://knowrob.org/kb/srdl2-comp.owl#', [keep(true)]).
 
 :-  rdf_meta
       assemblage_specified(r),
